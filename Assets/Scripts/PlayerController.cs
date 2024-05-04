8243e2e4933ad37797785915bb2ec571c084a9bf
marcio-t <a53581@alunos.ipb.pt>
2024-05-04 14:33:51 +0100

    adicionei um rigid body no veículo e no obstáculo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,6 @@ public class PlayerController : MonoBehaviour
     {
         // We´ll move the vehicle forward
         
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime *20);
     }
 }
