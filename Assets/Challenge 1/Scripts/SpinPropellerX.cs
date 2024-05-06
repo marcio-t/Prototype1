454aee79e0b548fbd67cc741a11b8f781b62d3ec
marcio-t <a53581@alunos.ipb.pt>
2024-05-06 12:30:13 +0100

    The camera is not following the plane

--- Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class SpinPropellerX : MonoBehaviour
 {
-    public GameObject plane;
-    private Vector3 offset = new Vector3(30, 0, 10);
+    private float propellorSpeed = 1000;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
     }
 }
